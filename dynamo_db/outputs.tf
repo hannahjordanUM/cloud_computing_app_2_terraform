911736e8554b1a3ef28c930016cc179941b0060e
hannahjordanUM <hannah.jordan1996@gmail.com>
2022-04-25 12:08:17 -0600

    removing directories

--- dynamo_db/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "dynamo_db_table_arn" {
-    value = aws_dynamodb_table.db_table.arn
-}
--- dynamo_db/variables.tf
+++
@@ -1,5 +0,0 @@
-
-variable "db_table_name" {
-    description = "dynamo db table name"
-    type = string
-} # from main.tf
